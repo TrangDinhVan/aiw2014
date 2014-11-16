48051b4638401144634374d473b53299038a6466
Trangdv <trangdv@outlook.com>
2014-11-16 22:43:15 +0700

    Update kid to template of club

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+aleticomadrid = Club.create! :name => 'Aletico Madrid', :year => '1903'
+bilbao_history = History.create! :name => "La Liga", :year => "1978, 1999, 2005, 2008, 2009", :club => aleticomadrid
+player1 = Player.create! :name => 'Miguel Angel Moya Rumbo', :leg => 'Phải', :club => aleticomadrid
