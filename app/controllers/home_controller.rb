ca95a0040cf52c4cd920d5244915e0ea429a4824
Trang Dinh Van <trangdv@outlook.com>
2014-12-02 22:35:57 +0700

    Return json object in web service

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,15 @@ class HomeController < ApplicationController
   respond_to :html, :xml, :json, :js
   def query
     if( params[:q] == 'clubs' )
-      render json: Club.get_query(params)
+      # render json: Club.get_query(params)
+      @players_set = Club.get_query(params)
+      @hash = {:clubs => @players_set}
+      render :json => @hash
     elsif (params[:q] == 'players')
-      render json: Player.get_query(params)
+      # render json: Player.get_query(params)
+      @clubs_set = Player.get_query(params)
+      @hash = {:players => @clubs_set}
+      render :json => @hash
     end
   end
 
